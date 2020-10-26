648613cb4cc307dca2637572d469af489fafdeac
xuyawen <xuyawenwy@163.com>
2020-10-26 19:27:45 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-content">
+        <code>App</code>
+      </div>
+    </div>
+  );
+}
+
+export default App;
